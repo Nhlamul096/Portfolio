026058a2b584383626f10a64b572eae37c1bbe4a
Onica Zikhali <ozikhali@environment.gov.za>
2024-09-25 11:01:42 +0200

    Initial commit for portfolio website

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    if (navLinks.style.display === 'block') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'block';
+    }
+}
