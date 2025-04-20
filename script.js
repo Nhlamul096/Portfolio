25974ad073978701e443c132fcf4c95bb07a937f
Onica Zikhali <ozikhali@environment.gov.za>
2025-04-20 20:20:47 +0200

    made major changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     if (navLinks.style.display === 'block') {
@@ -6,3 +7,20 @@ function toggleMenu() {
         navLinks.style.display = 'block';
     }
 }
+
+
+const roles = ["Developer", "Analyst", "Tester", "Trader"];
+let index = 0;
+const roleEl = document.getElementById("role");
+
+setInterval(() => {
+  roleEl.classList.remove("fade-in");
+  roleEl.classList.add("fade-out");
+
+  setTimeout(() => {
+    index = (index + 1) % roles.length;
+    roleEl.textContent = roles[index];
+    roleEl.classList.remove("fade-out");
+    roleEl.classList.add("fade-in");
+  }, 500);
+}, 2500);
